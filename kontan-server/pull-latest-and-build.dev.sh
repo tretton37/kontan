092a58ebb2cf95e171e14dbbcecec8f7a22d84b0
Englund <frainbreese@gmail.com>
2023-03-28 18:49:46 +0200

    change ngrok cmd

--- kontan-server/pull-latest-and-build.dev.sh
+++ kontan-server/pull-latest-and-build.dev.sh
@@ -8,6 +8,6 @@ npm run build:dev
 
 # Generate start scripts
 echo "export PATH="$(npm config get prefix)"/bin/node:$PATH" > startDev.sh
-{ echo "cd "$(pwd)""; echo "npm run serve:dev"; echo "ngrok http 8080 --log=stdout > ngrok.dev.log &"; } >> startDev.sh
+{ echo "cd "$(pwd)""; echo "npm run serve:dev"; echo "ngrok http localhost:8080 --log=stdout > ngrok.dev.log &"; } >> startDev.sh
 
 sudo supervisorctl restart kontan-dev
